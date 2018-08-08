df79e0284e4406e255fe624820f4c7ec540cab21
huxiao <huxiao@bytedance.com>
2018-08-09 01:06:40 +0800

    configure emacs

---
+++ editor/emacs/.emacs.d/init.el
@@ -0,0 +1,136 @@
+;; Turn off mouse interface early in startup to avoid momentary display
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen please... jeez
+(show-paren-mode 1)
+(global-hl-line-mode 1)
+(display-time-mode 1)
+(setq-default indent-tabs-mode nil)
+(setq inhibit-startup-screen t)
+(setq column-number-mode t)
+(setq line-number-mode t)
+(setq display-time-24hr-format t)
+(setq inhibit-startup-screen t)
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq show-paren-style 'parenthesis)
+(setq show-paren-style 'expression)
+(setq show-paren-style 'mixed)
+(setq max-mini-window-height 0.5)
+
+(add-to-list 'default-frame-alist '(font . "Pragmata Pro Mono-12"))
+
+;;;; package.el
+(require 'package)
+(setq package-user-dir "~/.emacs.d/elpa/")
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
+
+(defun mp-install-packages ()
+  "Install only the sweetest of packages."
+  (interactive)
+  (package-refresh-contents)
+  (mapc '(lambda (package)
+           (unless (package-installed-p package)
+             (package-install package)))
+        '(browse-kill-ring
+          magit
+          paredit
+          smex
+          evil
+          avy
+          company
+          irony
+          company-irony
+          rainbow-delimiters
+          ido-vertical-mode
+          clang-format
+          darcula-theme
+          undo-tree)))
+
+;;;; macros
+(defmacro after (mode &rest body)
+  "`eval-after-load' MODE evaluate BODY."
+  (declare (indent defun))
+  `(eval-after-load ,mode
+     '(progn ,@body)))
+
+;;;; global key bindings
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "C-x s") 'avy-goto-char-2)
+
+
+;;;; emacs lisp
+(defun imenu-elisp-sections ()
+  (setq imenu-prev-index-position-function nil)
+  (add-to-list 'imenu-generic-expression '("Sections" "^;;;; \\(.+\\)$" 1) t))
+
+(add-hook 'emacs-lisp-mode-hook 'imenu-elisp-sections)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (darcula)))
+ '(custom-safe-themes
+   (quote
+    ("3d5720f488f2ed54dd4e40e9252da2912110948366a16aef503f3e9e7dfe4915" default)))
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (darcula-theme clang-format ido-vertical-mode rainbow-delimiters company-irony irony company avy evil undo-tree smex paredit magit browse-kill-ring))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(defun reload-user-init-file()
+  (interactive)
+  (load-file user-init-file))
+
+
+(setq clang-format-style (concat "{BasedOnStyle: Google}"))
+
+(require 'evil)
+(evil-mode 1)
+
+(require 'undo-tree)
+(global-undo-tree-mode)
+
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on t)
+
+(after "rainbow-delimiters-autoloads"
+ (add-hook 'prog-mode-hook 'rainbow-delimiters-mode-enable))
+
+(progn
+  (require 'ido)
+  (ido-mode 1)
+  (if (version< emacs-version "25")
+      (progn
+        (make-local-variable 'ido-separator)
+        (setq ido-separator "\n"))
+    (progn
+      (make-local-variable 'ido-decorations)
+      (setf (nth 2 ido-decorations) "\n")))
+  (setq ido-enable-flex-matching t)
+  (setq ido-default-file-method 'selected-window)
+  (setq ido-default-buffer-method 'selected-window)
+  (define-key (cdr ido-minor-mode-map-entry) [remap write-file] nil))
+
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'c-mode-hook 'company-mode)
+(add-hook 'c++-mode-hook 'company-mode)
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
