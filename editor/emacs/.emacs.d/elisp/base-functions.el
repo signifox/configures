9c07077edcb7d59c733c288db3918b93305970d7
huxiao <huxiao@bytedance.com>
2018-09-09 15:07:44 +0800

    remove pdf for emacs

--- editor/emacs/.emacs.d/elisp/base-functions.el
+++ editor/emacs/.emacs.d/elisp/base-functions.el
@@ -113,25 +113,5 @@
   (setq google-translate-translation-directions-alist
         '(("en" . "zh-CN") ("zh-CN" . "en"))))
 
-(use-package pdf-tools
- :ensure t
- :config
- ;; initialise
- (pdf-tools-install)
- ;; open pdfs scaled to fit page
- (setq-default pdf-view-display-size 'fit-page)
- ;; automatically annotate highlights
- (setq pdf-annot-activate-created-annotations t)
- ;; use normal isearch
- (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward)
- ;; turn off cua so copy works
- (add-hook 'pdf-view-mode-hook (lambda () (cua-mode 0)))
- ;; more fine-grained zooming
- (setq pdf-view-resize-factor 1.1)
- ;; keyboard shortcuts
- (define-key pdf-view-mode-map (kbd "h") 'pdf-annot-add-highlight-markup-annotation)
- (define-key pdf-view-mode-map (kbd "t") 'pdf-annot-add-text-annotation)
- (define-key pdf-view-mode-map (kbd "D") 'pdf-annot-delete))
-
 
 (provide 'base-functions)
--- editor/emacs/.emacs.d/init.el
+++ editor/emacs/.emacs.d/init.el
@@ -66,7 +66,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
  '(package-selected-packages
    (quote
-    (pdf-tools counsel-projectile google-translate htmlize projectile dashboard markdown-mode elpy jedi company-jedi smart-mode-line smartparens powerline window-numbering rainbow-mode darkroom hydra dracula-theme company-irony-c-headers editorconfig ibuffer-sidebar yasnippet clang-format avy dired-sidebar yasnippet-snippets flycheck company-irony irony company smex magit counsel-gtags counsel swiper ivy evil-leader evil use-package)))
+    (counsel-projectile google-translate htmlize projectile dashboard markdown-mode elpy jedi company-jedi smart-mode-line smartparens powerline window-numbering rainbow-mode darkroom hydra dracula-theme company-irony-c-headers editorconfig ibuffer-sidebar yasnippet clang-format avy dired-sidebar yasnippet-snippets flycheck company-irony irony company smex magit counsel-gtags counsel swiper ivy evil-leader evil use-package)))
  '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
