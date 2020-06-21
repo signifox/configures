bc60e2c7e0a8ba842a74b1c0ed3759a495a69931
signifox <huxiao@bytedance.com>
2020-06-21 14:52:23 +0800

    add bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install coreutils vim zsh tmux clang-format ripgrep
+
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+###########
+#configure
+##########
+
+#https://github.com/signifox/configures/blob/master/vim/.vim/config.vim
+#https://github.com/signifox/configures/blob/master/.zshrc
+#https://github.com/signifox/configures/blob/master/.tmux.conf
+#https://github.com/signifox/configures/blob/master/.gitconfig
+
+############
+#software
+###########
+
+#https://www.cakebrew.com/
+#https://typora.io/
+#https://www.iterm2.com/
+#https://code.visualstudio.com/
+#https://sequelpro.com/test-builds
+#https://freemacsoft.net/appcleaner/
+
